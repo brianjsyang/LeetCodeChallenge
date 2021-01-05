7631f700d7bd45f8031f1151cc7c91cb5cb52643
brianjsyang <brianjsyang@gmail.com>
2021-01-04 17:28:31 -0800

    RunningSum1D

---
+++ RunningSum1D.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    for (i = 1; i < nums.length; i++){
+        nums[i] = nums[i-1] + nums[i];
+    }
+    
+    return nums;
+};
+
+console.log(runningSum);
